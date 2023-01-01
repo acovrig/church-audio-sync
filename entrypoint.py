8b7999b1f464bced7f28bcfcb3891b3140ea8cbb
Austin Covrig <accovrig@gmail.com>
2023-01-01 09:17:55 -0500

    Refactor

--- entrypoint.py
+++ entrypoint.py
@@ -1,8 +1,11 @@
 import sys, getopt, subprocess
 from os import path, walk, remove
 import re
-import pdb
 import autosub
+
+offset = None
+# Uncomment this to skip calculating offset:
+# offset = 0
 
 def print_help():
   print('-v <video file> -a <audio dir> [-c <chapters>] [-s <subtitles>] [-p <pdfs_dir] [-o <output>]')
@@ -31,74 +34,141 @@ def filter_file(f):
 
   try:
     cmd = ['ffmpeg', '-hide_banner', '-i', path.join(audio, f), '-af', 'volumedetect', '-f', 'null', path.join(audio, f'{f}.voldetect')]
-    print(f'Getting volume of {f}: ', end='')
-    # print(subprocess.list2cmdline(cmd))
+    try:
+      title=re.search(r"_([a-zA-Z0-9 ]*?)_Take", f).group(1)
+    except:
+      title = f
+    sys.stdout.write(f'Getting volume of {title}: ')
+    sys.stdout.flush()
     _, volume = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
     volume = re.search(r"max_volume: (.*?) dB", str(volume)).group(1)
     volume = float(volume)
-    print(volume)
+    print(volume, end='')
     if volume < -50:
+      print(' (ignore)')
       return False
   except:
+    print(' (include)')
     return True
+  print(' (include)')
   return True
 
-offset = None
-# Uncomment this to skip calculating offset:
-# offset = 0
+def find_sync_file():
+  global sync_file, rate
+  sync_file = None
+  for root, dirs, files in walk(audio):
+    for f in files:
+      if "stream" in f.lower():
+        sync_file = path.join(audio, f)
+        cmd = ['ffprobe', '-v', 'error', '-of', 'default=noprint_wrappers=1:nokey=1', '-show_entries', 'stream=sample_rate', path.join(audio, f)]
+        print('Sample rate: ', end='')
+        try:
+          rate, err = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()
+          rate = int(rate)
+          print(rate)
+          if rate != 48000:
+            if not path.exists(path.join(audio, f'{f}.sync.aac')):
+              cmd = ['ffmpeg', '-v', 'warning', '-stats', '-n', '-channel_layout', 'mono', '-i', path.join(audio, f), '-af', 'aresample=resampler=soxr', '-ar', '48000', path.join(audio, f'{f}.sync.aac')]
+              print('Converting rate: ', end='')
+              print(subprocess.list2cmdline(cmd))
+              process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+              for c in iter(lambda: process.stdout.read(1), b""):
+                sys.stdout.write(c)
+            sync_file = path.join(audio, f'{f}.sync.aac')
+            break
+        except:
+          print('ERROR: Unable to determine sample rate')
+          break
+  return sync_file
 
-video = ''
-chapters = ''
-audio = ''
-srt = ''
-pdfdir = ''
-output = ''
-try:
-  opts, args = getopt.getopt(sys.argv[1:],"hv:c:a:s:p:o:",["video=","chapters=", "audio_dir=", "subtitles=", "pdf_dir=", "output="])
-except getopt.GetoptError:
-  print_help()
-  sys.exit(2)
-for opt, arg in opts:
-  if opt == '-h':
+def get_opts():
+  global video, audio, chapters, srt, pdfdir, output
+  try:
+    opts, args = getopt.getopt(sys.argv[1:],"hv:c:a:s:p:o:",["video=","chapters=", "audio_dir=", "subtitles=", "pdf_dir=", "output="])
+  except getopt.GetoptError:
     print_help()
-    sys.exit()
-  elif opt in ("-v", "--video"):
-    video = arg
-  elif opt in ("-c", "--chapters"):
-    chapters = arg
-  elif opt in ("-a", "--audio_dir"):
-    audio = arg
-  elif opt in ("-s", "--subtitles"):
-    srt = arg
-  elif opt in ("-p", "--pdfs"):
-    pdfdir = arg
-  elif opt in ("-o", "--output"): 
-    output = arg
+    sys.exit(2)
+  for opt, arg in opts:
+    if opt == '-h':
+      print_help()
+      sys.exit()
+    elif opt in ("-v", "--video"):
+      video = arg
+    elif opt in ("-c", "--chapters"):
+      chapters = arg
+    elif opt in ("-a", "--audio_dir"):
+      audio = arg
+    elif opt in ("-s", "--subtitles"):
+      srt = arg
+    elif opt in ("-p", "--pdfs"):
+      pdfdir = arg
+    elif opt in ("-o", "--output"): 
+      output = arg
 
-if not path.exists(video):
-  print(f'ERROR: {video} does not exist')
-  sys.exit(2)
-if not path.exists(audio):
-  print(f'ERROR: {audio} does not exist')
-  sys.exit(2)
+  if not path.exists(video):
+    print(f'ERROR: {video} does not exist')
+    sys.exit(2)
+  if not path.exists(audio):
+    print(f'ERROR: {audio} does not exist')
+    sys.exit(2)
 
-if srt == '':
-  base = re.sub(r"\..{3}$", '', video)
-  srt = f'{base}.srt'
-  if not path.exists(srt):
-    srt = f'{video}.srt'
+  find_additions()
+  
+  return video, audio, chapters, srt, pdfdir, output
+
+def find_additions():
+  global srt, pdfdir, chapters
+  if srt == '':
+    base = re.sub(r"\..{3}$", '', video)
+    srt = f'{base}.srt'
     if not path.exists(srt):
-      print('Generating subtitles:')
-      autosub.generate_subtitles(source_path=video, output=f"{video}.srt")
-if pdfdir == '':
-  pdfdir = path.abspath(path.join(path.dirname(video), '..', 'pdf'))
-  if not path.exists(pdfdir):
-    pdfdir = None
-if chapters == '':
-  base = re.sub(r"\..{3}$", '', video)
-  chapters = f'{base}.chapters'
-  if not path.exists(chapters):
-    chapters = None
+      srt = f'{video}.srt'
+      if not path.exists(srt):
+        print('Generating subtitles:')
+        autosub.generate_subtitles(source_path=video, output=f"{video}.srt")
+  if pdfdir == '':
+    pdfdir = path.abspath(path.join(path.dirname(video), '..', 'pdf'))
+    if not path.exists(pdfdir):
+      pdfdir = None
+  if chapters == '':
+    base = re.sub(r"\..{3}$", '', video)
+    chapters = f'{base}.chapters'
+    if not path.exists(chapters):
+      chapters = None
+  return srt, pdfdir, chapters
+
+def find_offset():
+  global offset, sync_file
+  if offset == None:
+    offset, err = subprocess.Popen(['/src/compute-sound-offset.sh', video, sync_file, '900'], stdout=subprocess.PIPE).communicate()
+    if offset == b'':
+      print("Unable to determine offset, trying skip: ")
+      if path.exists(path.join(audio, f'{f}.sync.aac')):
+        cmd = ['ffmpeg', '-v', 'warning', '-stats', '-n', '-ss', str(30*60), '-i', path.join(audio, f'{f}.sync.aac'), '-c', 'copy', path.join(audio, f'{f}.sync30.aac')]
+      else:
+        cmd = ['ffmpeg', '-v', 'warning', '-stats', '-n', '-ss', str(30*60), '-i', path.join(audio, f), path.join(audio, f'{f}.sync30.aac')]
+      print('Jumping 30min: ', end='')
+      print(subprocess.list2cmdline(cmd))
+      process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+      for c in iter(lambda: process.stdout.read(1), b""):
+        sys.stdout.write(c)
+      sync_file = path.join(audio, f'{f}.sync30.aac')
+      offset, err = subprocess.Popen(['/src/compute-sound-offset.sh', video, sync_file, '900'], stdout=subprocess.PIPE).communicate()
+    if offset == b'':
+      print("Unable to determine offset, please manually enter (in seconds): ", end="")
+      sys.exit(2)
+  offset = float(offset)
+
+  for root, dirs, files in walk(audio):
+    for f in files:
+      if re.match(r".*stream.*aac$", f.lower()) != None:
+        print(f'REMOVE {f}')
+        remove(path.join(audio, f))
+  return offset
+
+video = chapters = audio = srt = pdfdir = output = ''
+rate = 0
+get_opts()
 
 print(f'Input video: {video}')
 print(f'Input audio dir: {audio}')
@@ -106,64 +176,15 @@ print(f'Input chapters: {chapters}')
 print(f'Input subtitles: {srt}')
 print(f'Input pdfs dir: {pdfdir}')
 print(f'Output: {output}')
+
 if chapters != None and not path.exists(chapters):
   print(f'ERROR: {chapters} does not exist')
   sys.exit(2)
 
-sync_file = None
-for root, dirs, files in walk(audio):
-  for f in files:
-    if "stream" in f.lower():
-      sync_file = path.join(audio, f)
-      cmd = ['ffprobe', '-v', 'error', '-of', 'default=noprint_wrappers=1:nokey=1', '-show_entries', 'stream=sample_rate', path.join(audio, f)]
-      print('Getting sample rate: ', end='')
-      print(subprocess.list2cmdline(cmd))
-      try:
-        rate, err = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()
-        rate = int(rate)
-        print(f'Sample rate: {rate}')
-        if rate != 48000:
-          if not path.exists(path.join(audio, f'{f}.sync.aac')):
-            cmd = ['ffmpeg', '-v', 'warning', '-stats', '-n', '-channel_layout', 'mono', '-i', path.join(audio, f), '-af', 'aresample=resampler=soxr', '-ar', '48000', path.join(audio, f'{f}.sync.aac')]
-            print('Converting rate: ', end='')
-            print(subprocess.list2cmdline(cmd))
-            process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-            for c in iter(lambda: process.stdout.read(1), b""):
-              sys.stdout.write(c)
-          sync_file = path.join(audio, f'{f}.sync.aac')
-          break
-      except:
-        print('ERROR: Unable to determine sample rate')
-        break
-
+find_sync_file()
 print(f'Sync file: {sync_file}')
 
-if offset == None:
-  offset, err = subprocess.Popen(['/src/compute-sound-offset.sh', video, sync_file, '900'], stdout=subprocess.PIPE).communicate()
-  if offset == b'':
-    print("Unable to determine offset, trying skip: ")
-    if path.exists(path.join(audio, f'{f}.sync.aac')):
-      cmd = ['ffmpeg', '-v', 'warning', '-stats', '-n', '-ss', str(30*60), '-i', path.join(audio, f'{f}.sync.aac'), '-c', 'copy', path.join(audio, f'{f}.sync30.aac')]
-    else:
-      cmd = ['ffmpeg', '-v', 'warning', '-stats', '-n', '-ss', str(30*60), '-i', path.join(audio, f), path.join(audio, f'{f}.sync30.aac')]
-    print('Jumping 30min: ', end='')
-    print(subprocess.list2cmdline(cmd))
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    for c in iter(lambda: process.stdout.read(1), b""):
-      sys.stdout.write(c)
-    sync_file = path.join(audio, f'{f}.sync30.aac')
-    offset, err = subprocess.Popen(['/src/compute-sound-offset.sh', video, sync_file, '900'], stdout=subprocess.PIPE).communicate()
-  if offset == b'':
-    print("Unable to determine offset, please manually enter (in seconds): ", end="")
-    sys.exit(2)
-offset = float(offset)
-
-for root, dirs, files in walk(audio):
-  for f in files:
-    if re.match(r".*stream.*aac$", f.lower()) != None:
-      print(f'REMOVE {f}')
-      remove(path.join(audio, f))
-
+find_offset()
 print(f'Offset: {offset}')
 
 if path.exists(output):
@@ -213,7 +234,10 @@ for i, f in enumerate(audio_files):
 cmd += [f'-metadata:s:a:0', 'title=AV']
 cmd += [f'-disposition:a:0', 'default']
 for i, f in enumerate(audio_files):
-  title=re.search(r"_([a-zA-Z0-9 ]*?)_Take", f).group(1)
+  try:
+    title=re.search(r"_([a-zA-Z0-9 ]*?)_Take", f).group(1)
+  except:
+    title = f
   cmd += [f'-metadata:s:a:{i+1}', f'title={title}']
 
 try:
@@ -231,11 +255,12 @@ except:
 
 cmd += ["-af", "aresample=resampler=soxr", "-ar", "48000"]
 
-cmd = ['ffprobe', '-v', 'error', '-of', 'default=noprint_wrappers=1:nokey=1', '-select_streams', 'v:0', '-show_entries', 'stream=codec_name', video]
-print('Getting video codec: ', end='')
-print(subprocess.list2cmdline(cmd))
+codec_cmd = ['ffprobe', '-v', 'error', '-of', 'default=noprint_wrappers=1:nokey=1', '-select_streams', 'v:0', '-show_entries', 'stream=codec_name', video]
+print('Video codec: ', end='')
 try:
-  codec, err = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()
+  codec, err = subprocess.Popen(codec_cmd, stdout=subprocess.PIPE).communicate()
+  codec = codec.decode('utf-8').replace("\n", "")
+  print(codec)
   if codec == 'hevc':
     cmd += ["-c:v", "copy"]
   else:
